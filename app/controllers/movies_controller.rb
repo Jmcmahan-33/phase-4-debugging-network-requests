85c1215bf1666840895ad053c664e79cc2f28316
Jared McMahan <jaredm788@gmail.com>
2023-03-26 11:24:09 -0600

    commit tree 25cb563c7c377283ca71023fa2e4f6067e5ec154

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,8 @@
 class MoviesController < ApplicationController
-  
+  def create
+    movie = Movie.create(movie_params)
+    render json: movie, status: :created
+  end
   def index
     movies = Movie.all
     render json: movies
